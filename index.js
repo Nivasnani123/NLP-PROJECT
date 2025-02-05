fabee44ad0fefb5b00a9d59cb08ba4b5282967de
Nivasnani123 <152023096+Nivasnani123@users.noreply.github.com>
2025-02-05 12:55:47 +0530

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('predictBtn').addEventListener('click', function() {
-    alert('Predict Sentiment button clicked!');
-    // You can add more functionality here, like redirecting to a prediction page or showing a modal.
-});
