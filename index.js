9d1a465628fbafe3acf78e9aca6d66d71589ceeb
PRASHANTH PENDEM <ismartprashi359@gmail.com>
2025-02-05 12:43:58 +0530

    app.py

---
+++ index.js
@@ -0,0 +1,4 @@
+document.getElementById('predictBtn').addEventListener('click', function() {
+    alert('Predict Sentiment button clicked!');
+    // You can add more functionality here, like redirecting to a prediction page or showing a modal.
+});
